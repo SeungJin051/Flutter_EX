34d8f9cf3a1de5ed7af073e061b42ed2ec888ce8
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-09-12 01:28:34 +0900

    StatefulWidget need Recap

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ex/screen/home_scree.dart';
 
 void main() {
-  // 프레임워크가 앱을 실행할 준비가 될때까지 기다린다.
-  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(
+    home: Root(),
+  ));
+}
 
-  runApp(
-    MaterialApp(
-      home: (HomeScreen()),
-    ),
-  );
+class Root extends StatefulWidget {
+  const Root({Key? key}) : super(key: key);
+
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  Color color = Colors.blue;
+  bool show = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: show
+                      ? HomeScreen(
+                          color: color,
+                        )
+                      : Container(),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        color = color == Colors.red ? Colors.blue : Colors.red;
+                      });
+                    },
+                    child: const Text(
+                      '색깔 변경하기',
+                    ),
+                  ),
+                  const SizedBox(height: 4.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        show = !show;
+                      });
+                    },
+                    child: Text(
+                      !show ? '위젯 생성하기' : '위젯 삭제하기',
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
